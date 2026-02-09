6aa06b2fb1c57eb8d7d6c5636035b46bd6f6f474
christoph goebel <christoph.goebel@netcentric.biz>
2026-02-09 15:16:17 +0100

    start basic layout approach for portfolio detail

--- src/portfolio-agent/src/views/Detail.vue
+++ src/portfolio-agent/src/views/Detail.vue
@@ -37,22 +37,113 @@ onMounted(async () => {
     />
   </main>
   <footer>
-    <div class="buttongroup">
-      <button class="button button--outline">Send to my email</button>
-      <button class="button">Explore similar cases</button>
-    </div>
+    <button class="button button--outline">Send to my email</button>
+    <button class="button">Explore similar cases</button>
   </footer>
 </template>
 
 <style>
 .detail-content {
   max-width: 100%;
-  padding: 0 1rem;
 }
 
 .detail-content img {
   max-width: 100%;
   display: block;
   height: auto;
+}
+
+/* section */
+.detail-content > div {
+  content: 'section';
+  max-width: 800px;
+  margin-inline: auto;
+}
+.detail-content > div + div {
+  margin-top: var(--sp-4);
+}
+
+/* typography */
+.detail-content h1 {
+  font-size: 70px;
+  line-height: 100%;
+  letter-spacing: 0;
+  vertical-align: middle;
+}
+
+.detail-content h2 {
+  font-size: 37.34px;
+  line-height: 44.8px;
+  letter-spacing: 0;
+  margin-bottom: var(--sp-2);
+}
+
+/* DA blocks */
+/*
+ TODO consider creating the markup for the stage
+  via javascript
+ */
+.detail-content > div:first-child {
+  width: 100%;
+  max-width: unset;
+  position: relative;
+}
+
+.detail-content > div:first-child > h1 {
+  position: absolute;
+  top: 55%;
+  left: var(--sp-2);
+  z-index: 10;
+  color: var(--white-100);
+}
+
+.textimage > div {
+  display: flex;
+  flex-direction: row;
+  gap: var(--sp-1);
+}
+
+.textimage > div > div {
+  flex: 0 1 48%;
+}
+
+.captionimage > div {
+  display: flex;
+  flex-direction: column;
+  gap: var(--sp-1);
+}
+
+.captionimage > div > div:last-child {
+  /* paragraph small */
+  font-weight: 400;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 24px;
+  letter-spacing: 0;
+  vertical-align: middle;
+}
+
+.textimage + .captionimage {
+  margin-top: var(--sp-2);
+}
+
+.cards {
+  display: grid;
+  gap: var(--sp-1);
+  grid-template-columns: 1fr 1fr;
+}
+
+.cards > div {
+  border-left: 3px solid var(--brand-primary);
+  padding: var(--sp-1) var(--sp-2);
+  background-color: var(--grey-15);
+}
+
+footer {
+  display: flex;
+  gap: var(--sp-1);
+  align-items: center;
+  justify-content: center;
+  padding-block: var(--sp-2);
 }
 </style>

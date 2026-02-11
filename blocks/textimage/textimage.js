1aaf24613317c8082511e6a4438cb7f2eb172a3d
Christian Daxberger <christian.daxberger@netcentric.biz>
2026-02-11 14:42:22 +0100

    Test textimage block

---
+++ blocks/textimage/textimage.js
@@ -0,0 +1,26 @@
+export default function decorate(block) {
+  const [leftDiv, rightDiv] = block.firstElementChild.children;
+
+  const orientation = leftDiv.querySelector('picture') ? 'left' : 'right';
+  block.classList.append(`textimage--orientation-${orientation}`);
+
+  const pictureDiv = orientation === 'left' ? leftDiv : rightDiv;
+  const textDiv = orientation === 'left' ? rightDiv : leftDiv;
+
+  const orgTitleH2Node = textDiv.querySelector('h2');
+  const titleH2 = null;
+  if (orgTitleH2Node) {
+    titleH2 = orgTitleH2Node.cloneNode(true);
+    orgTitleH2Node.remove();
+  }
+
+  block.innerHTML = `
+    ${titleH2 ? `<header>${titleH2.outerHTML}</header>` : ''}
+    <div class="textimage__image">
+        ${pictureDiv.innerHTML}
+    </div>
+    <div class="textimage__text">
+        ${textDiv.innerHTML}
+    </div>
+  `;  
+}

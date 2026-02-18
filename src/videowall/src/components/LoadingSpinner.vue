43850848314a61bbfef5ef0506ace8b3c7c8c7c3
Christian Daxberger <christian.daxberger@netcentric.biz>
2026-02-18 13:57:00 +0100

    Preparation Videowall app

---
+++ src/videowall/src/components/LoadingSpinner.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  Video Player
+</template>
+
+<style scoped>
+</style>
---
+++ src/videowall/src/components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  }
+})
+
+</script>
+
+<template>
+  <video class="videoplayer__video">
+    <source :src="props.src" type="video/mp4">
+  </video>
+</template>
+
+<style scoped>
+</style>

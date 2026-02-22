02d152ef485dab5ca4c4d2813d39a1c7cc1e701b
klevi.mehmeti <klevi.mehmeti@netcentric.biz>
2026-02-22 13:59:50 +0100

    Add overlay in the image selected

--- src/photo-booth/src/views/DeliveryOptions.vue
+++ src/photo-booth/src/views/DeliveryOptions.vue
@@ -4,6 +4,16 @@
 
         <div class="image-wrapper">
             <img :src="demo.selectedPhoto" />
+
+            <!-- Overlay Frame -->
+            <div class="frame-overlay">
+                <div class="frame-inner">
+                    <span class="corner top-left"></span>
+                    <span class="corner top-right"></span>
+                    <span class="corner bottom-left"></span>
+                    <span class="corner bottom-right"></span>
+                </div>
+            </div>
         </div>
 
         <div class="actions">
@@ -50,11 +60,63 @@ function sendEmail() {
 
 .image-wrapper img {
     width: 100%;
-    border-radius: 16px;
 }
 
 .actions {
     display: flex;
     gap: 1rem;
+}
+
+/* frame sits OUTSIDE the photo a bit */
+.frame-overlay {
+    position: absolute;
+    inset: -16px;
+    pointer-events: none;
+    z-index: 2;
+}
+
+.frame-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: 16px;
+}
+
+.corner {
+    position: absolute;
+    width: 28px;
+    height: 28px;
+    border-color: #26EFE9;
+    border-style: solid;
+    border-width: 0;
+}
+
+/* corners */
+.top-left {
+    top: -2px;
+    left: -2px;
+    border-top-width: 4px;
+    border-left-width: 4px;
+}
+
+.top-right {
+    top: -2px;
+    right: -2px;
+    border-top-width: 4px;
+    border-right-width: 4px;
+}
+
+.bottom-left {
+    bottom: -2px;
+    left: -2px;
+    border-bottom-width: 4px;
+    border-left-width: 4px;
+}
+
+.bottom-right {
+    bottom: -2px;
+    right: -2px;
+    border-bottom-width: 4px;
+    border-right-width: 4px;
 }
 </style>

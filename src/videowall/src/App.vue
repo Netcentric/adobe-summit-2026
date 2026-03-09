c6d24c67edc49dce7045b4d6f7aa32dc27693892
christoph goebel <christoph.goebel@netcentric.biz>
2026-03-09 17:19:31 +0100

    add driver simulation

--- src/videowall/src/App.vue
+++ src/videowall/src/App.vue
@@ -1,35 +1,56 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
 import config from './config.ts';
+import { faker } from '@faker-js/faker';
+
+interface DriverRaw {
+  image: string;
+  video: string;
+  uid: string;
+  name: string;
+  course: string;
+  year: number;
+}
 
 interface Driver {
-  name: string;
-  url: string;
+  image: string;
+  video: string;
   uid: string;
+  name: string;
+  course: string;
+  year: number;
+  fetched: number;
+  played: number[];
 }
 
 const drivers = ref<Driver[]>([]);
+// proxy for simulating mapping driver
+const createDriver = (): Driver => ({
+  uid: faker.string.uuid(),
+  image: 'string',
+  video: 'string',
+  name: faker.person.fullName(),
+  course: faker.location.city(),
+  year: faker.number.int({ min: 1910, max: 2060 }),
+  fetched: Date.now(),
+  played: [],
+});
 
 const updateDrivers = () => {
   try {
     // await fetch drivers
     // ....
 
-    drivers.value = [
-      {
-        name: 'nonalia',
-        url: 'https://video.com',
-        uid: '1',
-      },
-    ];
+    drivers.value = [...drivers.value, createDriver()];
+
     console.log('updateDrivers', drivers.value);
   } catch {
     console.error('Drivers not found');
   }
 };
 
+// polling interval
 let interval: undefined | number;
-
 onMounted(() => {
   interval = setInterval(() => updateDrivers(), config.POLL_INTERVAL_MS);
 });
@@ -40,7 +61,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <pre>{{ JSON.stringify(drivers) }}</pre>
+  <pre style="white-space: pre">{{ JSON.stringify(drivers, null, 4) }}</pre>
 </template>
 
 <style scoped></style>

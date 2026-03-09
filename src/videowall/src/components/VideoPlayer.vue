826e72403d15f2457319b4636998e489c52ffd66
christoph goebel <christoph.goebel@netcentric.biz>
2026-03-09 18:42:23 +0100

    add "unplayed" batch queue

--- src/videowall/src/components/VideoPlayer.vue
+++ src/videowall/src/components/VideoPlayer.vue
@@ -1,20 +1,39 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue';
+import type { Driver } from '../App.vue';
 
-const props = defineProps({
-  src: {
-    type: String,
-    required: true,
+const props = defineProps<{ driver: Driver | null }>();
+
+const emits = defineEmits(['start', 'stop']);
+
+const currentDriver = ref<Driver | null>(null);
+
+watch(props, (curr) => {
+  // emits('stop', currentDriver);
+  currentDriver.value = curr.driver || null;
+
+  if (currentDriver.value) {
+    emits('start');
+
+    // simulate playtime
+    setTimeout(() => {
+      emits('stop', currentDriver.value);
+    }, 6000);
   }
-})
-
+});
 </script>
 
 <template>
-  <video class="videoplayer__video">
-    <source :src="props.src" type="video/mp4">
+  <video
+    class="videoplayer__video"
+    v-if="currentDriver"
+  >
+    <source
+      :src="currentDriver.video"
+      type="video/mp4"
+    />
   </video>
+  <p v-else>no driver</p>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>

22551589c400822474385ff4d6053db69cee043f
christoph goebel <christoph.goebel@netcentric.biz>
2026-03-16 13:45:48 +0100

    add route parameter handling for setting api token

--- src/videowall/src/main.ts
+++ src/videowall/src/main.ts
@@ -2,4 +2,13 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+// handle token parameter
+const searchParams = new URLSearchParams(location.search);
+
+if (searchParams.get('t')) {
+  localStorage.setItem('token', `token ${searchParams.get('t')}`);
+  location.replace(location.origin);
+}
+
+// mount app
 createApp(App).mount('#app');

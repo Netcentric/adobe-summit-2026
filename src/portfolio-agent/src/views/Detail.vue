8f3c3d0bf75868daa489577a588d8059eaabb162
christoph goebel <christoph.goebel@netcentric.biz>
2026-04-14 09:48:44 +0200

    add finally block, reformat (sigh)

--- src/portfolio-agent/src/views/Detail.vue
+++ src/portfolio-agent/src/views/Detail.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
-import { computed, inject, nextTick, onMounted, provide, ref, useTemplateRef, watch, type Ref } from 'vue';
+import {
+  computed,
+  inject,
+  nextTick,
+  onMounted,
+  provide,
+  ref,
+  useTemplateRef,
+  watch,
+  type Ref,
+} from 'vue';
 import { useRoute } from 'vue-router';
 import Stage from '../components/EDS/Stage.vue';
 import Section from '../components/EDS/Section.vue';
@@ -12,9 +22,15 @@ const edsIsLoading = ref(false);
 const edsDocument = ref<Document | null>(null);
 const edsError = ref<any | null>(null);
 const edsMetaData = ref<Map<string, string>>(new Map());
-const pageTitle = computed(() => edsDocument.value?.querySelector('h1')?.innerText);
-const edsStageNode = computed(() => edsDocument.value?.querySelector('body > main > div'));
-const edsSectionNodes = computed(() => edsDocument.value?.querySelectorAll('body > main > div:not(:first-child)'));
+const pageTitle = computed(
+  () => edsDocument.value?.querySelector('h1')?.innerText
+);
+const edsStageNode = computed(() =>
+  edsDocument.value?.querySelector('body > main > div')
+);
+const edsSectionNodes = computed(() =>
+  edsDocument.value?.querySelectorAll('body > main > div:not(:first-child)')
+);
 const edsFoundTaglistKeys = ref();
 const edsFirstSectionIntersectingIndex = ref(-1);
 const edsActiveNavigationJumpLinkIndex = ref(-1);
@@ -23,14 +39,10 @@ const intersectingSectionIndexes = ref<Array<number>>([]);
 
 const currentLocation = ref<string>('');
 
-const qrcode = useQRCode(
-  currentLocation,
-  {
-    errorCorrectionLevel: 'H',
-    margin: 3,
-  }
-);
-
+const qrcode = useQRCode(currentLocation, {
+  errorCorrectionLevel: 'H',
+  margin: 3,
+});
 
 const showShareButton = computed(() => navigator.share !== undefined);
 
@@ -48,11 +60,18 @@ const edsNavigationJumpLinks = computed(() => {
   }
   const links: JumpLink[] = Array.from(edsSectionNodes.value)
     .map((sec, index) => {
-      const hasIntroScreen = index > 0 && edsSectionNodes.value?.item(index - 1).querySelector('div.introslide') ? true: false;
+      const hasIntroScreen =
+        index > 0 &&
+        edsSectionNodes.value?.item(index - 1).querySelector('div.introslide')
+          ? true
+          : false;
       const startSectionIndex = hasIntroScreen ? index - 1 : index;
       const headline = sec.querySelector('h2')?.innerText.trim() || '';
       const headlineMobileShortened = headline.replace(/^the /i, '');
-      const headlineMobile = headlineMobileShortened ? headlineMobileShortened.substring(0, 1).toUpperCase() + headlineMobileShortened.substring(1) : '';
+      const headlineMobile = headlineMobileShortened
+        ? headlineMobileShortened.substring(0, 1).toUpperCase() +
+          headlineMobileShortened.substring(1)
+        : '';
       return {
         headline,
         headlineMobile,
@@ -61,7 +80,7 @@ const edsNavigationJumpLinks = computed(() => {
         endSectionIndex: startSectionIndex,
       };
     })
-    .filter(section => section.headline.length > 0);
+    .filter((section) => section.headline.length > 0);
   links.forEach((link, index, arr) => {
     if (!edsSectionNodes.value) {
       return;
@@ -71,7 +90,7 @@ const edsNavigationJumpLinks = computed(() => {
     } else {
       link.endSectionIndex = edsSectionNodes.value.length - 1;
     }
-  })
+  });
   return links;
 });
 
@@ -80,53 +99,64 @@ provide('edsSectionNodes', edsSectionNodes);
 provide('edsNavigationJumpLinks', edsNavigationJumpLinks);
 provide('edsActiveNavigationJumpLinkIndex', edsActiveNavigationJumpLinkIndex);
 
-watch(edsUrl, async () => {
-  edsIsLoading.value = true;
-  edsDocument.value = null;
-  edsError.value = null;
-  edsMetaData.value.clear();
+watch(
+  edsUrl,
+  async () => {
+    edsIsLoading.value = true;
+    edsDocument.value = null;
+    edsError.value = null;
+    edsMetaData.value.clear();
 
-  contentRef.value?.scrollTo({
-    top: 0,
-  });
-
-  try {
-    const response = await fetch(edsUrl.value);
-    if (response.ok) {
-      const responseSource = await response.text();
-      const parser = new DOMParser();
-      edsFoundTaglistKeys.value = [];
-      const replaceTagsSource = responseSource.replace(/\[taglist(?:\:([^\]]+))?\]/g, (_, listKey) => {
-        const key = listKey || '';
-        if (!edsFoundTaglistKeys.value.includes(key)) {
-          edsFoundTaglistKeys.value.push(key);
-        }
-        return `<my-taglist metaKey="${key}"></my-taglist>`;
-      });
-      edsDocument.value = parser.parseFromString(replaceTagsSource, 'text/html');
-      intersectingSectionIndexes.value = [];
-      updateActiveSection();
-      
-      edsReadMetadata();
-      if (route.query?.autoScroll) {
-        startAutoScroll();
-      }
-    }
-  } catch (e) {
-    edsError.value = e;
-  }
-  edsIsLoading.value = false;
-
-  // wait for next tick and scroll to top of page.
-  nextTick(() => {
     contentRef.value?.scrollTo({
       top: 0,
     });
-    currentLocation.value = window.location.href.replace(/\?(.*)$/, '');
-  })
-}, {
-  immediate: true,
-});
+
+    try {
+      const response = await fetch(edsUrl.value);
+      if (response.ok) {
+        const responseSource = await response.text();
+        const parser = new DOMParser();
+        edsFoundTaglistKeys.value = [];
+        const replaceTagsSource = responseSource.replace(
+          /\[taglist(?:\:([^\]]+))?\]/g,
+          (_, listKey) => {
+            const key = listKey || '';
+            if (!edsFoundTaglistKeys.value.includes(key)) {
+              edsFoundTaglistKeys.value.push(key);
+            }
+            return `<my-taglist metaKey="${key}"></my-taglist>`;
+          }
+        );
+        edsDocument.value = parser.parseFromString(
+          replaceTagsSource,
+          'text/html'
+        );
+        intersectingSectionIndexes.value = [];
+        updateActiveSection();
+
+        edsReadMetadata();
+        if (route.query?.autoScroll) {
+          startAutoScroll();
+        }
+      }
+    } catch (e) {
+      edsError.value = e;
+    } finally {
+      edsIsLoading.value = false;
+    }
+
+    // wait for next tick and scroll to top of page.
+    nextTick(() => {
+      contentRef.value?.scrollTo({
+        top: 0,
+      });
+      currentLocation.value = window.location.href.replace(/\?(.*)$/, '');
+    });
+  },
+  {
+    immediate: true,
+  }
+);
 
 watch(route, () => {
   edsUrl.value = `/portfolio-agent/${route.params.id}`;
@@ -144,7 +174,7 @@ if (humanPresent) {
     if (humanPresent.value) {
       stopAutoScroll();
     }
-  })
+  });
 }
 
 let autoScrollInterval: number | undefined;
@@ -181,9 +211,9 @@ function autoScrollNextSection() {
   }
   const target = sections[autoScrollSection];
   target?.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-  })
+    behavior: 'smooth',
+    block: 'start',
+  });
 }
 
 function getTagsByKey(metaKey?: string): Array<string> {
@@ -191,30 +221,35 @@ function getTagsByKey(metaKey?: string): Array<string> {
     return [
       ...getTagsByKey('industries'),
       ...getTagsByKey('field-of-interest'),
-    ]
+    ];
   }
-  return edsMetaData.value.get(metaKey)
+  return (
+    edsMetaData.value
+      .get(metaKey)
       ?.split(',')
-      .map(t => t.trim())
-      .filter(t => t.length > 0) || [];
+      .map((t) => t.trim())
+      .filter((t) => t.length > 0) || []
+  );
 }
 
 function edsReadMetadata() {
   edsMetaData.value.clear();
-  edsDocument.value?.documentElement.querySelectorAll('head > meta').forEach((metaNode) => {
-    if (
-      !metaNode.hasAttribute('name')
-      || !metaNode.hasAttribute('content')
-      || !metaNode.getAttribute('name')?.trim().length
-    ) {
-      return;
-    }
+  edsDocument.value?.documentElement
+    .querySelectorAll('head > meta')
+    .forEach((metaNode) => {
+      if (
+        !metaNode.hasAttribute('name') ||
+        !metaNode.hasAttribute('content') ||
+        !metaNode.getAttribute('name')?.trim().length
+      ) {
+        return;
+      }
 
-    edsMetaData.value?.set(
-      metaNode.getAttribute('name') || '',
-      metaNode.getAttribute('content') || ''
-    );
-  });
+      edsMetaData.value?.set(
+        metaNode.getAttribute('name') || '',
+        metaNode.getAttribute('content') || ''
+      );
+    });
 }
 
 function onSectionIntersecting(sectionIndex: number) {
@@ -223,7 +258,9 @@ function onSectionIntersecting(sectionIndex: number) {
 }
 
 function onSectionNotIntersecting(sectionIndex: number) {
-  intersectingSectionIndexes.value = intersectingSectionIndexes.value.filter(sI => sI != sectionIndex);
+  intersectingSectionIndexes.value = intersectingSectionIndexes.value.filter(
+    (sI) => sI != sectionIndex
+  );
   updateActiveSection();
 }
 
@@ -233,8 +270,15 @@ function updateActiveSection() {
     edsActiveNavigationJumpLinkIndex.value = -1;
     return;
   }
-  edsFirstSectionIntersectingIndex.value = Math.max(...intersectingSectionIndexes.value);
-  edsActiveNavigationJumpLinkIndex.value = edsNavigationJumpLinks.value?.findIndex(link => link.startSectionIndex <= edsFirstSectionIntersectingIndex.value && link.endSectionIndex >= edsFirstSectionIntersectingIndex.value) || 0;
+  edsFirstSectionIntersectingIndex.value = Math.max(
+    ...intersectingSectionIndexes.value
+  );
+  edsActiveNavigationJumpLinkIndex.value =
+    edsNavigationJumpLinks.value?.findIndex(
+      (link) =>
+        link.startSectionIndex <= edsFirstSectionIntersectingIndex.value &&
+        link.endSectionIndex >= edsFirstSectionIntersectingIndex.value
+    ) || 0;
 }
 
 function onShare() {
@@ -243,32 +287,60 @@ function onShare() {
   }
   navigator.share({
     title: pageTitle.value,
-    url: window.location.href
+    url: window.location.href,
   });
 }
 </script>
 
 <template>
-  <div class="detail-content" ref="content">
+  <div
+    class="detail-content"
+    ref="content"
+  >
     <div class="hidden">
-      <div data-taglist :data-taglist-key="taglistKey" v-for="taglistKey in edsFoundTaglistKeys">
-        <TagList :tags="getTagsByKey(taglistKey)" variant="outline"/>
+      <div
+        data-taglist
+        :data-taglist-key="taglistKey"
+        v-for="taglistKey in edsFoundTaglistKeys"
+      >
+        <TagList
+          :tags="getTagsByKey(taglistKey)"
+          variant="outline"
+        />
       </div>
     </div>
-    <Stage v-if="edsStageNode" :node="edsStageNode" :tags="getTagsByKey()"></Stage>
+    <Stage
+      v-if="edsStageNode"
+      :node="edsStageNode"
+      :tags="getTagsByKey()"
+    ></Stage>
     <Section
       :node="sectionNode"
       :index="sectionIndex"
       @intersecting="onSectionIntersecting(sectionIndex)"
       @not-intersecting="onSectionNotIntersecting(sectionIndex)"
       v-for="(sectionNode, sectionIndex) in edsSectionNodes"
-      :key="sectionIndex"/>
+      :key="sectionIndex"
+    />
     <footer v-if="showShareButton">
-      <div v-if="kioskMode && qrcode" class="detail__qrcode">
-        Scan to open on mobile:<br/>
-        <img class="share_qrCode" :src="qrcode" alt="QR Code">
+      <div
+        v-if="kioskMode && qrcode"
+        class="detail__qrcode"
+      >
+        Scan to open on mobile:<br />
+        <img
+          class="share_qrCode"
+          :src="qrcode"
+          alt="QR Code"
+        />
       </div>
-      <button v-else-if="!kioskMode && showShareButton" @click="onShare" class="button button--outline">Share this case</button>
+      <button
+        v-else-if="!kioskMode && showShareButton"
+        @click="onShare"
+        class="button button--outline"
+      >
+        Share this case
+      </button>
       <!-- <button class="button">Explore similar cases</button> -->
     </footer>
   </div>
@@ -300,7 +372,7 @@ function onShare() {
     letter-spacing: 0;
     vertical-align: middle;
 
-    @include bp-min ($bp-tablet) {
+    @include bp-min($bp-tablet) {
       font-size: 70px;
       line-height: 100%;
     }
@@ -312,7 +384,7 @@ function onShare() {
     letter-spacing: 0;
     margin-bottom: var(--sp-1);
 
-    @include bp-min ($bp-tablet) {
+    @include bp-min($bp-tablet) {
       margin-bottom: var(--sp-2);
     }
   }
@@ -358,5 +430,4 @@ html {
     display: inline-block;
   }
 }
-
 </style>

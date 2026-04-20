a9c165c66221840f4260dc3ff3d1d75be5136e57
Andreas Haller <andreas.haller@netcentric.biz>
2026-04-20 09:22:38 -0700

    temporal

--- src/videowall/src/main.ts
+++ src/videowall/src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { Temporal } from '@js-temporal/polyfill';
+
+// This makes it available globally if your code expects 'Temporal' to exist
+if (!globalThis.Temporal) {
+  globalThis.Temporal = Temporal;
+}
 
 // handle token parameter
 const searchParams = new URLSearchParams(location.search);
